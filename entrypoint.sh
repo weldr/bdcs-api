3a119f65473f0f83b6117703a8872049edb2a5c9
Henry Wang <henrywangxf@me.com>
2018-07-03 22:32:39 +0800

    Fix docker container socket issue. (#83)
    1. The bdcs-api docker image is able to work with domain socket.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ STORE=$(realpath /mddb/"${STORE:-cs.repo}")
 MDDB="/mddb/${MDDB:-metadata.db}"
 RECIPES="${RECIPES:-/recipes/}"
 
-/usr/local/bin/bdcs-api-server --bdcs "$STORE" "$MDDB" "$RECIPES"
+mkdir -p /run/weldr
+/usr/local/bin/bdcs-api-server -s /run/weldr/api.socket -g root --bdcs "$STORE" "$MDDB" "$RECIPES"
